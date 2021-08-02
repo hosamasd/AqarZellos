19750618a8438dc6100242a90320654687f59174
hosam <hosamelmalt@gmail.com>
2021-08-02 06:38:22 +0200

    Initial Commit

---
+++ AqarZellos/AqarZellosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AqarZellosApp.swift
+//  AqarZellos
+//
+//  Created by hosam on 02/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct AqarZellosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
